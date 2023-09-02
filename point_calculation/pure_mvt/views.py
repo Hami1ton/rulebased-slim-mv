1a6f1bee857a0016df18a17f6d86a4806f944370
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-09-02 21:13:27 +0900

    add view and templates

--- point_calculation/pure_mvt/views.py
+++ point_calculation/pure_mvt/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello.")
+    template = loader.get_template("sample.html")
+    context = {
+        "latest_question_list": "",
+    }
+    return render(request, "sample.html", context)
